97cfdec6a4228afc032cc42acf38b1cb5c7db5f7
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-05-05 03:04:22 +0300

    feat: blank design system added tailwind preset

--- apps/playground/src/views/HomeView.vue
+++ apps/playground/src/views/HomeView.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import { Button } from "@blank/vue";
+import { Button } from '@blank/vue'
 </script>
 
 <template>
-  <main>
+  <main class="h-screen w-full">
     <Button> Hello world </Button>
-    <div class="bg-red-500">asd</div>
+    <div>
+      <h1>test</h1>
+      <h2>test</h2>
+      <h3>test</h3>
+      <h4>test</h4>
+      <h5>test</h5>
+      <h6>test</h6>
+      <p>test</p>
+      <a href="#">test</a>
+    </div>
   </main>
 </template>
--- packages/vue/src/components/Button/Button.vue
+++ packages/vue/src/components/Button/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="bg-primary">
+  <button class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white font-sans">
     <slot />
   </button>
 </template>
